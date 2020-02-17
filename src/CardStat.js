685e8bee11b801d0b999bc31558e214daef29360
princelewis <princelewis4christ@yahoo.com>
2020-02-17 19:59:01 +0100

    Create form for analytics

--- src/CardStat.js
+++ src/CardStat.js
@@ -4,6 +4,11 @@ import styled from "styled-components";
 export default function CardStat() {
   return (
     <Component>
+      <h2>ANALYTICS</h2>
+      <p>Checkout the statistics of all the ATM cards that have been checked on our site</p>
+      <br></br>
+      <br></br>
+
       <form>
         <label for="start">Start:</label>
         <input type="text" placeholder="Enter start number" id="start" />
@@ -14,13 +19,13 @@ export default function CardStat() {
       <div className="stat__cover">
         <div className="top__cover">
           <div className="right__top">
-            <h3>Start:</h3>
-            <span className="span">1</span>
+            <h3>Card Numbers:</h3>
+            <span className="span">565376738</span>
           </div>
 
-          <div className="right__top">
-            <h3>limit:</h3>
-            <span className="span">2</span>
+          <div className="left__top">
+            <h3>Size:</h3>
+            <span className="span">27734894 79384883737</span>
           </div>
         </div>
       </div>
@@ -29,11 +34,23 @@ export default function CardStat() {
 }
 
 const Component = styled.div`
+
+  h2{
+    text-align: center;
+    font-weight: bolder;
+    font-size: 2.5rem;
+    margin-bottom: 0px;
+  }
+  p{
+    text-align: center;
+    margin-top: 0px;
+
+  }
   form {
     margin: 0px auto;
+    width: 39%;
   }
 
-  text-align: center;
   label {
     margin: 0px 15px;
     font-weight: bold;
@@ -55,9 +72,32 @@ const Component = styled.div`
       justify-content: space-between;
 
   }
+  .bordered{
+    border: 1px solid black;
+  }
 
   .right__top{
-      display: flex;
+      /* display: flex; */
+      width: 50%;
       align-content: center;
+      padding: 5px;
+  }
+  .left__top{
+    width: 30%;
+    padding: 5px;
+    text-align: right;
+  }
+
+  .stat__cover{
+    width: 39%;
+    margin: 0px auto;
+    margin-top: 20px;
+    padding: 25px 15px;
+    border-radius: 15px;
+    box-shadow: 5px 8px 22px -9px #251d3d;
+    background-color: white;
+  }
+  span{
+    display: block;
   }
 `;
